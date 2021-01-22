3199177b5de3edf8a78ad015ae8c7e3ccc06e518
EtienneUrbano <urbano.etienne@laposte.net>
2021-01-22 17:33:52 +0100

    Amélioration de l'affichage

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -16,24 +16,25 @@ public class DeclarationApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("- Je ne suis pas un héro ! ");
-		System.out.println("- Mais alors, qui êtes-vous ? ");
-		System.out.println("- Un mec lambda. Et vous ? \n- Un dev qui vous veut du bien =)");
+		System.out.println("- Je ne suis pas un héro ! \n- Mais alors, qui êtes-vous ? ");
+		System.out.println("- Un mec lambda. Et vous ? \n- ...Un dev qui vous veut du bien =)\n");
 
-		byte a = 16;
-		short b = 213;
-		int c = 1234;
-		long d = 1234L;
-		float e = 1234f;
+		byte 	a = 53;
+		short 	b = 213;
+		int 	c = 1234;
+		long 	d = 1234L;
+		float 	e = 1234f;
 		e *= 3.141592;
-		e -= b * d;
-		double f = 1234e34;
-		char g = '\\';
+		e -= b * d; 	// e devient négatif
+		double 	f = 1234e34;
+		char 	g = '\\';
 		boolean h = 666 > c;
-		String i = "Qui est-ce ? --> " + g;
+		String 	i = "Qui est là ? --> " + g + ". C'est Slash. Ses " + c 
+				+ " clones sont répartis dans ton code. C'est pas " + h + ".";
+	
 		System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
-		System.out.println("e = " + e + ", f = " + f + ", g = " + g + ", h = " + h + ", i = " + i);
-		System.out.println("Etat d'avancement du projet :\n9 tâches sur 10 sont terminées");
+		System.out.println("e = " + e + ", f = " + f + ", g = " + g + ", h = " + h + ",\ni = \"" + i + "\"");
+		System.out.println("\nEtat d'avancement du projet :\n9 tâches sur 10 sont terminées");
 	}
 
 }
