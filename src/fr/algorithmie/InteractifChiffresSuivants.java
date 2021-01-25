1241d772e57259c855d3e3d3db530774314e543d
EtienneUrbano <urbano.etienne@laposte.net>
2021-01-25 16:10:19 +0100

    Ajout InteractifTantQue, InteractifTableMult, InteractifChiffresSuivants

---
+++ src/fr/algorithmie/InteractifChiffresSuivants.java
@@ -0,0 +1,21 @@
+package fr.algorithmie;
+
+import java.util.Scanner;
+
+public class InteractifChiffresSuivants {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Saisir un entier relatif (positif ou négatif): ");
+		int nb = scanner.nextInt();
+
+		for (int i = nb + 1; i <= nb + 10; i++) {
+			if (i == nb + 10) {
+				System.out.println(i + ".");
+			} else {
+				System.out.print(i + ", ");
+			}
+		}
+	}
+}
