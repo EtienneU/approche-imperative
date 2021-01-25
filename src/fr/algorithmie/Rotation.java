3e30222b0c942a6d73021d08bba27ad1c3b490fa
EtienneUrbano <urbano.etienne@laposte.net>
2021-01-25 15:50:56 +0100

    Ajout Rotation

---
+++ src/fr/algorithmie/Rotation.java
@@ -0,0 +1,27 @@
+package fr.algorithmie;
+
+public class Rotation {
+
+	public static void main(String[] args) {
+
+		int[] array = new int[] { -9, 45, 6, 23, -74 };
+		int[] rotatedArray = new int[array.length];
+
+		rotatedArray[0] = array[array.length - 1];
+		int i = 1;
+		while (i < rotatedArray.length) {
+			rotatedArray[i] = array[i - 1];
+			i++;
+		}
+
+		// vérification par l'affichage :
+		System.out.print("Tableau initial :      array = ");
+		for (int j = 0; j < array.length; j++) {
+			System.out.print(array[j] + " ");
+		}
+		System.out.print("\nTableau final : rotatedArray = ");
+		for (int j = 0; j < rotatedArray.length; j++) {
+			System.out.print(rotatedArray[j] + " ");
+		}
+	}
+}
