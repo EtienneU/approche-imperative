e7473a6021bf7a50e91f0ec81fd686a3ece3e814
EtienneUrbano <urbano.etienne@laposte.net>
2021-01-25 15:05:25 +0100

    Ajout SommeDeTableaux, SommeDeTableauxDiff et ComparaisonTableau

---
+++ src/fr/algorithmie/ComparaisonTableau.java
@@ -0,0 +1,33 @@
+package fr.algorithmie;
+
+public class ComparaisonTableau {
+
+	public static void main(String[] args) {
+		
+		int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4};
+		int[] array2 = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8};
+		
+		// récuperation des deux longueurs de tableau
+//		int maxLength = (array1.length > array2.length) ? array1.length : array2.length;
+//		int minLength = (array1.length > array2.length) ? array2.length : array1.length;
+		
+		int[] bigTab = array1;
+		int[] smallTab = array2;
+		if (array1.length < array2.length) {
+			bigTab = array2;
+			smallTab = array1;
+		}
+		
+		int count = 0; // nombre d'éléments en commun dans mes deux tableaux
+		
+		for (int i = 0; i < bigTab.length; i++) {
+			for (int j = 0; j < smallTab.length; j++) {
+				if (bigTab[i] == smallTab[j]) {
+					count++;
+				}
+			}
+		}
+		
+		System.out.println("Nombre d'éléments communs à mes 2 tableaux : " + count);
+	}
+}
---
+++ src/fr/algorithmie/SommeDeTableauxDiff.java
@@ -0,0 +1,37 @@
+package fr.algorithmie;
+
+public class SommeDeTableauxDiff {
+
+	public static void main(String[] args) {
+
+		int[] array1 = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+		int[] array2 = { -1, 12, 17, 14, 5, -9, 0, 18 };
+
+		int[] bigTab = array1;
+		int[] smallTab = array2;
+		// petite méthode permettant de récupérer la référence du plus grand tab et
+		// celle du plus court
+		if (array1.length < array2.length) {
+			bigTab = array2;
+			smallTab = array1;
+		}
+
+		// déclaration de mon tableau final qui recevra les sommes de chaque élément de
+		// tab1 et tab2
+		int[] arraySom = new int[bigTab.length];
+
+		for (int i = 0; i < arraySom.length; i++) {
+			if (i <= smallTab.length - 1) {
+				arraySom[i] = bigTab[i] + smallTab[i];
+			} else {
+				arraySom[i] = bigTab[i];
+			}
+		}
+
+		// vérification par l'affichage :
+		System.out.print("Somme des éléments de mes deux tableaux : ");
+		for (int i = 0; i < arraySom.length; i++) {
+			System.out.print(arraySom[i] + " ");
+		}
+	}
+}
